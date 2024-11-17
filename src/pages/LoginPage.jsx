f76a8c5f14435b0783513e6f00a698b6e7ec8b4e
Hritik Yadav <144812620+Azmodeus07@users.noreply.github.com>
2024-11-17 15:12:55 +0530

    First Task
    Completed the first task of adding
    Signup page, Login Page, Main Page, Dashboard Page, Journal Page
    Now all left in them is to fine tune them .

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import Login from "../components/Auth/Login";
+
+const LoginPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
